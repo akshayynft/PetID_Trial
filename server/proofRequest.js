f8fb23616ccf0fef450a7be48a6de1cef76d38a5
kagade0211 <kagadeakshay0211@gmail.com>
2023-07-14 07:15:34 +0530

    created basic template to start pet ID

--- server/proofRequest.js
+++ server/proofRequest.js
@@ -1,16 +1,15 @@
 const { KYCAgeCredential } = require("./vcHelpers/KYCAgeCredential");
 
-// design your own customised authentication requirement here using Query Language
-// https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
+const humanReadableAuthReason = "Must be born before today and this time and be the owner of a pet";
 
-const humanReadableAuthReason = "Must be born before this year";
+const currentDate = new Date();
+const currentDateTime = currentDate.toISOString();
 
 const credentialSubject = {
   birthday: {
-    // users must be born before this year
-    // birthday is less than Jan 1, 2023
-    $lt: 20230101,
+    $lt: currentDateTime, // Ensure birthday is less than the current date and time
   },
+  isOwnerOfPet: true, // Set to true if the user is the owner of a pet
 };
 
 const proofRequest = KYCAgeCredential(credentialSubject);
